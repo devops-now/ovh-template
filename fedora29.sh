6d6bef67c14f3d3a71d18fb98d0a64e576d1b98d
Aurélien Rouëné <aurelien@devops-now.com>
2019-02-22 19:41:43 +0100

    Create fedora29.sh

---
+++ fedora29.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+dnf upgrade -y
+dnf install -y lvm2 policycoreutils-python-utils ansible langpacks-fr
+
+# Enable root ssh
+sed -i 's/,command.*ssh-rsa/ ssh-rsa/' /root/.ssh/authorized_keys
+sed -i 's/#\?\s*Port\s\+.*/Port 225/' /etc/ssh/sshd_config
+
+semanage port -a -t ssh_port_t -p tcp 225
+
+# Use LVM
+umount /home
+pvcreate -y /dev/md3
+vgcreate lvm /dev/md3
+lvcreate -L 1G -n home lvm
+mkfs.ext4 /dev/lvm/home
+
+sed -i 's/LABEL="\/home" \/home xfs/\/dev\/mapper\/lvm-home \/home ext4/' /etc/fstab
+
+# Fix boot LV on MDRaid
+rm /etc/dracut.conf.d/98-ovh.conf
+dracut -f
+
+# Manage users
+userdel -r fedora
+
+# Use selinux
+sed -i "s/#\?\s*SELINUX\s*=.*/SELINUX=permissive/" /etc/selinux/config
+touch /.autorelabel
+
+# Disable cloud-init
+touch /etc/cloud/cloud-init.disabled
+
+# Fix boot of mdmonitor
+sed -i '1i MAILADDR root' /etc/mdadm.conf
+
+exit 0
